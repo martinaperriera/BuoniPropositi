fb56ccefcc909181d9d3b46351da139280c2fb0f
User1 <User1@macbook-air.home>
2024-06-14 10:01:07 +0200

    v1

--- src/org/gen/italy/Main.java
+++ src/org/gen/italy/Main.java
@@ -11,13 +11,13 @@ Si continua a chiedere all’utente fino a quando tutti i propositi saranno
 conseguiti.
  */
 
-import java.util.Scanner; 
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		ArrayList<String> buoniPropositi = new ArrayList<String>();
 		buoniPropositi.add("Fare più attività fisica.");
 		buoniPropositi.add("Esercitarsi sulla programmazione orientata agli oggetti.");
@@ -25,30 +25,32 @@ public class Main {
 		buoniPropositi.add("Leggere più libri.");
 		buoniPropositi.add("Risparmiare.");
 
-		int checkProposito; //per indicare il proposito conseguito
-		
-		//dowhile + for che si ripete finché la lista dei buoni propositi non è completata (isEmpty)
+		int checkProposito; // per indicare il proposito conseguito
+
+		// dowhile + for che si ripete finché la lista dei buoni propositi non è
+		// completata (isEmpty)
 		do {
 			System.out.println("Lista buoni propositi");
-			for (int i=0; i<buoniPropositi.size(); i++) {
-				System.out.println((i+1) + " - " + buoniPropositi.get(i));
+			for (int i = 0; i < buoniPropositi.size(); i++) {
+				System.out.println((i + 1) + " - " + buoniPropositi.get(i));
 			}
-				System.out.println("Inserisci il numero del proposito conseguito (1-5)");
-				checkProposito=sc.nextInt()-1;
-				sc.nextLine();
-				
-				if (checkProposito>=0 && checkProposito<buoniPropositi.size()) {
-					buoniPropositi.remove(checkProposito);
-					System.out.println("Complimenti! Sei sempre più vicinoa al tuo obiettivo.");
+			System.out.println("Inserisci il numero del proposito conseguito (1-5)");
+			checkProposito = sc.nextInt() - 1;
+			sc.nextLine();
 
-				} else {
-					System.out.println("Inserimento non valido. Riprova!!");
-				}
-				
-		}while(!buoniPropositi.isEmpty());
-		
-		//stampo uscita dal ciclo
-		System.out.println("Complimenti! Hai completato la lista dei buoni propositi. Keep up the good work e al prossimo anno! ;)");
+			if (checkProposito >= 0 && checkProposito < buoniPropositi.size()) {
+				buoniPropositi.remove(checkProposito);
+				System.out.println("Complimenti! Sei sempre più vicinoa al tuo obiettivo.");
+
+			} else {
+				System.out.println("Inserimento non valido. Riprova!!");
+			}
+
+		} while (!buoniPropositi.isEmpty());
+
+		// stampo uscita dal ciclo
+		System.out.println(
+				"Complimenti! Hai completato la lista dei buoni propositi. Keep up the good work e al prossimo anno! ;)");
 		sc.close();
 
 	}
